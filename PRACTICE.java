b5ce1bd7b131eac7dbded13d04247a38f0ae21a5
Plashchavan <113756208+Plashchavan@users.noreply.github.com>
2022-09-29 18:35:34 +0530

    Add files via upload

---
+++ PRACTICE.java
@@ -0,0 +1,55 @@
+package JAVA;
+
+public class PRACTICE {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+	    int A=12;
+		int B=0;
+		
+		for(int i=1; i<=A; i++)
+		{
+			
+			if(A%i==0)
+			{
+				B++;
+			}
+			
+		}
+		
+		if (B==2)
+		System.out.println("It is prime number");
+		else
+			System.out.println("Is not a prime number");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+//		int A=9;
+//	    int B=0;
+//	    for(int i=1; i<=A ; i++)
+//	    {
+//	      if(A%i==0)
+//	      {
+//	        B++;
+//	      }
+//	    }
+//	    if (B==2)
+//	      System.out.println("it is prime number");
+//	    else
+//	      System.out.println("is not prime number");
+//		
+		
+	}
+}
